071e62578ecdf9c1634c267dfec1c355cbe80ee0
bharani215 <162295767+bharani215@users.noreply.github.com>
2026-01-29 17:19:25 +0530

    Update settings.py

--- num_game/settings.py
+++ num_game/settings.py
@@ -151,11 +151,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT=os.path.join(BASE_DIR,'static/media')
 
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
